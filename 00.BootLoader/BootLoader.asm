e4ba587ba468812f9cc0c54f2bc6de8b182b669e
hangstone <hangstonee@gmail.com>
2017-05-10 20:24:09 +0900

    temporary chapter 5.2.1

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -52,6 +52,71 @@ MESSAGE1: db 'HANG64 OS Boot Loader was Started!', 0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;	chapter 5.2.1
+
+TOTALSECTORCOUNT:		dw		1024		;;	bootloader를 제외한  OS 이미지 크기
+																	;;	최대 1152 sector(0x90000 byte)까지 가능
+SECTORNUMBER:				db		0x02		;;	OS 이미지가 시작하는 섹터 번호를 저장하는 영역
+HEADNUMBER:					db		0x00		;;	OS 이미지가 시작하는 헤드 번호를 저장하는 영역
+TRACKNUMBER:				db		0x00		;;	OS 이미지가 시작하는 트랙 번호를 지정하는 영역
+
+	;;	디스크 내용을 메모리로 복사할 address(ES:BX)를 0x10000으로 설정
+	mov si, 0x1000									;;	OS image를 복사할 adress(0x10000)를 세그먼트 레지스터 값으로 변환
+	mov es, si											;;	ES 세그먼트 레지스터 값으로 변환
+	mov bx, 0x0000									;;	BX 레지스터에 0x0000을 설정하여 복사할 address를
+																	;;	0x1000:0000(0x10000)으로 최종 설정
+	mov di, word[TOTALSECTORCOUNT]	;;	복사할 OS 이미지의 섹터 수를 DI 레지스터에 설정
+
+READDATA:			;;	디스크를 읽는 코드
+	;;	모든 섹터를 다 읽었는지 확인
+	cmp	di, 0				;;	복사할 OS 이미지의 섹터 수를 '0'과 비교
+	je  READEND			;;	복사할 sector 수가 '0'이라면, 다 복사한 것이므로 READEND로 이동
+	sub di, 0x01		;;	복사할 sector 수를 '1' 감소
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;	Call a BIOS Read Function
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	mov	ah, 0x02								;;	BIOS 서비스 #2 (read sector)
+	mov al, 0x01								;;	읽을 섹터 수는 '1'
+	mov ch, byte[TRACKNUMBER]		;;	읽을 트랙 번호 설정
+	mov cl, byte[SECTORNUMBER]	;;	읽을 섹터 번호 설정
+	mov dh, byte[HEADNUMBER]		;;	읽을 헤드 번호 설정
+	mov dl, 0x00								;;	읽을 드라이브 번호(floppy = 0) 설정
+	int 0x13										;;	interrupt service 수행
+;;	jc HANDLEDISKERROR					;; 	에러가 발생 했다면, HANDLEDISKERROR로 이동
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;	Calculate address to copy, track, head and sector address
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	add si, 0x0020			;;	512-byte(0x200) 만큼 읽었으므로, 이를 세그먼트 레지스터 값으로 이동
+	mov es, si					;;	ES 세그먼트 레지스터에 더해서, address를 한 섹터만큼 증가
+
+	;;	한 섹터를 읽었으므로 섹터 번호를 증가시키고, 마지막 섹터(18)까지 읽었는지 판단
+	;;	마지막 섹터가 아니면, 섹터 읽기로 이동해서 다시 섹터 읽기 수행
+	mov al, byte[SECTORNUMBER]	;;	섹터 번호를 AL 레지스터에 설정
+	add al, 0x01								;;	섹터 번호를 '1' 증가
+	mov byte[SECTORNUMBER], al	;;	증가시킨 섹터 번호를 SECTORNUMBER에 다시 설정
+	cmp al, 19									;;	증가시킨 섹터 번호를 '19'와 비교
+	jl READDATA									;;	섹터 번호가 '19'미만이라면, READDATA로 이동
+
+	;;	마지막 섹터까지 읽었으면(즉, 섹터 번호가 '19'이면), 헤드를 toggle(0->1, 1->0)하고,
+	;;	섹터 번호를 '1'로 설정
+	xor byte[HEADNUMBER], 0x01		;;	헤드 번호를 0x01과 XOR하여 toggle
+	mov byte[SECTORNUMBER], 0x01	;;	섹터 번호를 다시 '1'로 설정
+
+	;;	만약 헤드가 1->0 으로 바뀌었으면, 양쪽 헤드를 모두 읽은 것이므로 아래로 이동하여 track 번호를 '1' 증가
+	cmp byte[HEADNUMBER], 0x00		;;	헤드 번호를 0x00과 비교
+	jne READDATA									;;	헤드 번호가 '0'이 아니라면 READDATA로 이동
+
+	;;	track을 '1' 증가시킨 후, 다시 섹터 읽기로 이동
+	add byte[TRACKNUMBER], 0x01		;;	트랙 번호를 '1'증가
+	jmp READDATA									;;	READDATA로 이동
+READEND:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 jmp $					;;	현재 위치에서 infinitt loop 수행
 
 times 510 - ($ - $$) db 0x00	;;	$: 현재 line의 address
